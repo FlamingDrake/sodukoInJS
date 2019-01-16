8a6fa2d9bf0202b487de9a07d26fae9194a24c3d
FlamingDrake <robin.ljungstrom@gmail.com>
2019-01-16 14:14:41 +0100

    Now generating a matrix with size 9x9 filled with 0s. it ait pretty, but its there!

--- js/generator.js
+++ js/generator.js
@@ -1,5 +1,25 @@
 
+const totalColumns = 9;
+const totalRows = 9;
+
 function generate() {
-    let sodokuDiv = document.getElementById("generateMe")
-    sodokuDiv.innerText("hello")
+    let sodokuDiv = document.getElementById("generateMe");
+    sodokuDiv.innerHTML = "";
+    let sodokuMatrix = document.createElement("table");
+
+    for(let i = 0; i < totalRows; i++){
+        let row = document.createElement("tr");
+
+
+        for(let j = 0; j < totalColumns; j++){
+            let cell = document.createElement("td");
+            //TODO Generate a solvable sodoku here. Also make sure the cells that generate with empty cells are changeable.
+            let cellText = document.createTextNode("0");
+
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
+        sodokuMatrix.appendChild(row);
+    }
+    sodokuDiv.appendChild(sodokuMatrix);
 }
