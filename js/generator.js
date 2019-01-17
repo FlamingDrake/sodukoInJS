9925a5c14a6c7d49dc28c084385fc753ac54d50c
Mackan <olsson-1994@hotmail.com>
2019-01-17 14:18:48 +0100

    cell id

--- js/generator.js
+++ js/generator.js
@@ -17,6 +17,7 @@ function generate() {
             let cell = document.createElement("td");
             //TODO Generate a solvable sodoku here. Also make sure the cells that generate with empty cells are changeable.
             let cellText = document.createTextNode("0");
+            cell.setAttribute("id", "cell"+i+"."+j);
 
             cell.appendChild(cellText);
             row.appendChild(cell);
@@ -24,6 +25,28 @@ function generate() {
         sodokuMatrix.appendChild(row);
     }
     sodokuDiv.appendChild(sodokuMatrix);
+}
+function validateSudoko(){
+    let validCells = true;
+    let value = 0;
+    for(let i = 0; i < totalRows; i++){
+        for(let j = 0; j < totalColumns; j++){
+            value = document.getElementById("cell"+i+"."+j).value;
+            console.log(value);
+            // check row
+            for(let k = i; k < totalRows; k++){
+                if(document.getElementById("cell"+k+"."+j).value == value){
+                    console.log("check: " + document.getElementById("cell"+k+"."+j).value);
+                    validCells = false;
+                }
+                else{
+
+                }
+            }
+            // check colum
+            // check box
+        }
+    }
 }
 
 function generateValidSoduko() {
