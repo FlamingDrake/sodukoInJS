22c084dad2050ed050a4f841e38c72f060a27e64
FlamingDrake <robin.ljungstrom@gmail.com>
2019-01-17 13:24:04 +0100

    It now generates 1-9 on each row... Getting there

--- js/generator.js
+++ js/generator.js
@@ -1,6 +1,7 @@
-
 const totalColumns = 9;
 const totalRows = 9;
+
+let currRow = 0;
 
 function generate() {
     let sodokuDiv = document.getElementById("generateMe");
@@ -9,11 +10,11 @@ function generate() {
 
     let solvedSoduko = generateValidSoduko();
 
-    for(let i = 0; i < totalRows; i++){
+    for (let i = 0; i < totalRows; i++) {
         let row = document.createElement("tr");
 
 
-        for(let j = 0; j < totalColumns; j++){
+        for (let j = 0; j < totalColumns; j++) {
             let cell = document.createElement("td");
             //TODO Generate a solvable sodoku here. Also make sure the cells that generate with empty cells are changeable.
             let cellText = document.createTextNode(solvedSoduko[i][j]);
@@ -31,25 +32,25 @@ function generateValidSoduko() {
 
     let validNumbers = new Array(totalRows);
 
-    for(let i = 1; i <= totalRows; i++){
-        validNumbers[i-1] = i;
+    for (let i = 1; i <= totalRows; i++) {
+        validNumbers[i - 1] = i;
     }
 
     function validateArray(validNumbers, matrix, row, col) {
-        for(let i = 0; i < totalRows; i++){
-            if(matrix[col][i] !== 0){
+        for (let i = 0; i < totalRows; i++) {
+            if (matrix[col][i] !== 0) {
                 validNumbers[matrix[col][i]] = 0;
             }
         }
-        for(let i = 0; i < totalColumns; i++){
-            if(matrix[i][row] !== 0){
+        for (let i = 0; i < totalColumns; i++) {
+            if (matrix[i][row] !== 0) {
                 validNumbers[matrix[i][row]] = 0;
             }
         }
         let returnValue = 0;
 
-        for(let i = 0; i < validNumbers.length; i++){
-            if(validNumbers[i] !== 0){
+        for (let i = 0; i < validNumbers.length; i++) {
+            if (validNumbers[i] !== 0) {
                 return validNumbers[i];
             }
         }
@@ -57,60 +58,62 @@ function generateValidSoduko() {
         return returnValue;
     }
 
-    function putNumber(matrix, col) {
-        /*
-        if (col < 0 || col >= totalColumns || row < 0 || row >= totalRows) {
-            return false;
-        }
-        putNumber(matrix, row+1,col);
-        putNumber(matrix, row,col+1);
+    function generateOptions(matrix, col, row) {
+        let set = new Set();
 
-        if (matrix[col][row] === 0){
-            matrix[col][row] = validateArray(validNumbers,matrix,row,col);
+        for (let i = 0; i < totalColumns; i++) {
+            if (matrix[i][row] !== 0) {
+                set.add(matrix[i][row]);
+            }
+        }
+
+        for (let i = 0; i < totalRows; i++) {
+            if (matrix[col][i] !== 0) {
+                set.add(matrix[col][i]);
+            }
+        }
+
+        let returnSet = new Set();
+
+
+        for (let i = 0; i < validNumbers.length; i++) {
+            if (!set.has(validNumbers[i])) {
+                returnSet.add(validNumbers[i])
+            }
+        }
+
+        return returnSet;
+    }
+
+
+    function putNumber(matrix, col) {
+
+        if (col === totalColumns) {
+            currRow++;
+            col = 0;
+        }
+        if (currRow === totalRows) {
             return true;
         }
-        return false;
-        */
 
-        for (let row = 0; row < totalRows; row++) {
-            if (col === totalColumns && row === totalRows) {
-                return true;
-            }
+        let options = generateOptions(matrix, col, currRow);
 
-            if (validateArray()) {
-                return false;
-            }
 
+        options.forEach(cellOption => {
+            matrix[col][currRow] = cellOption;
             if (putNumber(matrix, col + 1)) {
                 return true;
             }
-        }
 
+        });
 
-/*
-        if(matrix[col][row] === 0){
-            matrix[col][row] = validateArray(validNumbers, matrix, row, col);
-            if(col < totalColumns-1){
-                putNumber(matrix, row, col+1);
-            } else if (row < totalRows-1){
-                putNumber(matrix, row+1, 0);
-            } else {
-                return true;
-            }
-        } else {
-            if(col < totalColumns-1){
+        matrix[col][currRow] = 0;
+        return false;
 
-                putNumber(matrix, row, col+1);
-            } else if (row < totalRows-1){
-                putNumber(matrix, row+1, 0);
-            } else{
-                return true;
-            }
-        }
-        */
     }
 
-    putNumber(matrix, 0, 0);
+    currRow = 0;
+    putNumber(matrix, 0,);
 
     return matrix;
 }
